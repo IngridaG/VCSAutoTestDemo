8fb8537a9342849db96cf5e96f7a1f5a05405618
ingrida <ingrida.gurskyte@gmail.com>
2022-10-11 08:36:15 +0300

    pom package/ SeleniumEasy test class

---
+++ src/test/java/pom/tests/seleniumEasy/FirstFormDemoTest.java
@@ -0,0 +1,46 @@
+package pom.tests.seleniumEasy;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pom.pages.seleniumEasy.FirstFormDemoPage;
+import pom.tests.TestBase;
+
+public class FirstFormDemoTest extends TestBase {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        FirstFormDemoPage.open(
+                "https://demo.seleniumeasy.com/basic-first-form-demo.html"
+        );
+    }
+
+    @Test
+    private void testInputFieldSeleniumEasy() {
+        String expectedFullName = "Dailius";
+        String actualFullName;
+
+        FirstFormDemoPage.enterFullName(expectedFullName);
+        FirstFormDemoPage.clickButtonShowMessage();
+        actualFullName = FirstFormDemoPage.readMessage();
+
+        Assert.assertEquals(actualFullName, expectedFullName);
+    }
+
+    @Test
+    private void testTwoInputFieldsAdd5and5Values(){
+        String input1 = "5";
+        String input2 = "5";
+        String expectedSumTotal = "10";
+        String actualSumTotal;
+
+        FirstFormDemoPage.enterValueA(input1);
+        FirstFormDemoPage.enterValueB(input2);
+        FirstFormDemoPage.clickButtonGetTotal();
+        actualSumTotal = FirstFormDemoPage.readSumTotal();
+
+        Assert.assertEquals(actualSumTotal,expectedSumTotal );
+    }
+}
