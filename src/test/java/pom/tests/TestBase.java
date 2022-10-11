7b373ed0d1c0597aa39a9aca4234566448ef24cb
ingrida <ingrida.gurskyte@gmail.com>
2022-10-11 08:41:39 +0300

    pom package/ TesBase general class

---
+++ src/test/java/pom/tests/TestBase.java
@@ -0,0 +1,19 @@
+package pom.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pom.pages.seleniumEasy.FirstFormDemoPage;
+
+public class TestBase {
+
+    @BeforeMethod
+    public void setUp() {
+        FirstFormDemoPage.setUp();
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        FirstFormDemoPage.close();
+    }
+}
